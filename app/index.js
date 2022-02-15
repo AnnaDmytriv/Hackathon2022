7bc1622261c413599ff5b6b75f93bedbbecad0b8
anna.dmytriv <anna.dmytriv@user1st.com>
2022-02-15 17:37:57 +0200

    db

--- app/index.js
+++ app/index.js
@@ -1,5 +1,28 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
-    
+    saveErrorInDB();
 
 });
+
+const saveErrorInDB = () => {
+    const elWithError = document.querySelector("img[src='images/5.webp']").parentElement;
+
+    const bodyRect = document.body.getBoundingClientRect(),
+    const elmRect = elWithError.getBoundingClientRect(),
+    const absX = elmRect.left - bodyRect.left;
+    const absY = elmRect.top - bodyRect.top;
+
+    const error = {
+        tag: elWithError.tagName,
+        attributes: elWithError.attributes,
+        parents: elWithError.parentElement.tagName,
+        children: elWithError.children.length,
+        content: elWithError.innerHTML,
+        position: {
+            x: absX,
+            y: absY
+        }
+    }
+
+    localStorage.setItem("Errors", [JSON.stringify(error)]);
+};
