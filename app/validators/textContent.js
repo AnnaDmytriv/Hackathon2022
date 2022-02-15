9f3b4f53a45b3e3a1c1180cc09adef59d14de4a5
daniel-ilan <daniel.ilan222@gmail.com>
2022-02-15 18:41:55 +0200

    added children

---
+++ app/validators/textContent.js
@@ -0,0 +1,30 @@
+const getTextScore = (el, dataObj) => {
+  let score = 0;
+  const attrs = element.attributes ? [...element.attributes] : [];
+
+  score = someAttrNames(attrs, dataObj);
+
+  return score;
+};
+
+const someAttrNames = (attrs, dataObj) => {
+  let similarAttrsScore;
+  let similarAttrsValueScore;
+  const attrScore = 5 / attrs.length;
+  const { attributes } = dataObj;
+
+  attrs.forEach((attr) => {
+    if (dataObj.attributes.findIndex(attr)) {
+      if (sameAttrValue(attr, attributes[idx])) {
+        similarAttrsValueScore += attrScore;
+      }
+      similarAttrsScore += attrScore;
+    }
+  });
+
+  return similarAttrsScore + similarAttrsValueScore;
+};
+
+const sameAttrValue = (attr1, attr2) => {
+  return attr1.value === attr2.value;
+};
