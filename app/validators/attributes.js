6ea1b49b929de1b362ccbfbbfd73d7548b3f8526
daniel-ilan <daniel.ilan222@gmail.com>
2022-02-15 19:26:11 +0200

    diff between all attrs and class attrs

--- app/validators/attributes.js
+++ app/validators/attributes.js
@@ -10,19 +10,24 @@ export const getAttrsScore = (el, dataObj) => {
 const someAttrNames = (attrs, dataObj) => {
   let similarAttrsScore;
   let similarAttrsValueScore;
-  const attrScore = 50 / attrs.length;
+  const attrScore = 75 / attrs.length;
+  const classScore = 25 / attrs.length;
   const { attributes } = dataObj;
 
-  attrs.forEach((attr) => {
-    if (dataObj.attributes.findIndex(attr)) {
+  const maxScore = attrScore * attrs.length + classScore * attrs.length;
+
+  attrs.forEach((attr, idx) => {
+    if (attributes.find(attr)) {
+      const scoreToAdd = attr.name === 'class' ? classScore : attrScore;
+      similarAttrsScore += classScore;
       if (sameAttrValue(attr, attributes[idx])) {
-        similarAttrsValueScore += attrScore;
+        similarAttrsValueScore += scoreToAdd;
       }
-      similarAttrsScore += attrScore;
+      similarAttrsScore += scoreToAdd;
     }
   });
 
-  return similarAttrsScore + similarAttrsValueScore;
+  return (similarAttrsScore + similarAttrsValueScore) / maxScore;
 };
 
 const sameAttrValue = (attr1, attr2) => {
